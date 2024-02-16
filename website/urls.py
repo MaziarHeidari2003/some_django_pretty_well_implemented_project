585a9b891b97a811e931f168245e93a0df791b60
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-02-16 13:47:28 -0800

    first commit

---
+++ website/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'website'
+
+urlpatterns = [
+  path('', views.index_view, name='index' ),
+  path('/about', views.about_view, name='about' ),
+  path('/contact', views.contact_view, name='contact' ),
+  path('test', views.test_view, name='test')
+
+
+]
