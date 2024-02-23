e018fc511d0fd24954fb4815a8fbf46a54f5f6ad
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-02-23 15:23:59 -0800

    some custom filter tag is done, truncatechar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,7 +1,18 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
 
-@register.simple_tag
-def hello():
-  return 'hello'
+@register.simple_tag(name='total')
+def total_posts():
+  posts = Post.objects.all()
+  return posts.count()
+
+@register.simple_tag(name='posts')
+def posts():
+  posts = Post.objects.all()
+  return posts
+
+@register.filter
+def snippets(value,args=50):
+  return value[:args] + '...'
