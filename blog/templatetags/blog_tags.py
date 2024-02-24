9c825db7cd8a1e264a05d6443039759c8ffda821
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-02-24 12:54:19 -0800

    blog tags compeleting

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post,Category
 
 register = template.Library()
 
@@ -16,3 +16,17 @@ def posts():
 @register.filter
 def snippets(value,args=50):
   return value[:args] + '...'
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def popularposts(args=1):
+  posts = Post.objects.filter(status=1).order_by('published_date')[:args]
+  return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-category.html')
+def postcategories():
+  posts = Post.objects.filter(status=1) 
+  categories = Category.objects.all() 
+  cat_dict = {}
+  for name in categories:
+     cat_dict[name] = posts.filter(category=name).count() 
+  return {'categories':cat_dict}   
