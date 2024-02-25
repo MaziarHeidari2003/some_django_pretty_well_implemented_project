6f8b6f5d277d9c6ff26185b043dccf50606f3bce
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-02-25 12:44:38 -0800

    end of the season 7, the pagination is done

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
   path('<int:pid>', views.blog_single, name='single' ),
   path('<int:pid>', views.detail_post, name='detail_post'),
   path('category/<str:cat_name>', views.blog_view, name='category'),
-  path('author/<str:author_username>', views.blog_view, name='author')
+  path('author/<str:author_username>', views.blog_view, name='author'),
+  path('search/',views.blog_search, name='search')
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 from datetime import datetime
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -12,6 +13,15 @@ def blog_view(request,**kwargs):
   if kwargs.get('author_username') != None:
     posts = posts.filter(author__username=kwargs['author_username'])
 
+  posts = Paginator(posts,2)
+  try:
+    page_number = request.GET.get('page')
+    posts = posts.get_page(page_number)
+  except PageNotAnInteger:
+    posts = posts.get_page(1)
+  except EmptyPage:
+    posts = posts.get_page(1)
+  
   return render(request, 'blog/blog-home.html', {
     'posts': posts
   })
@@ -53,6 +63,15 @@ def next_post(request, pid):
 def blog_category(request,cat_name):
   posts = Post.objects.filter(status=1)
   posts = posts.filter(category__name=cat_name)
+  return render(request, 'blog/blog-home.html', {
+    'posts':posts
+  })
+
+def blog_search(request):
+  posts = Post.objects.filter(status=1)
+  if request.method == 'GET':
+    if s:= request.GET.get('s'):
+      posts = posts.filter(content__contains=s)
   return render(request, 'blog/blog-home.html', {
     'posts':posts
   })
