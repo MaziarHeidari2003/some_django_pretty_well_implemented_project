60ebd63cc570bc3d64081ed617a5b9b89d6504ca
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-02-28 02:46:01 -0800

    so im stuglling with sitemaps and robots

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
   path('<int:pid>', views.detail_post, name='detail_post'),
   path('category/<str:cat_name>', views.blog_view, name='category'),
   path('author/<str:author_username>', views.blog_view, name='author'),
-  path('search/',views.blog_search, name='search')
+  path('search/',views.blog_search, name='search'),
 
 
 ]
