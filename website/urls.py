7a492a8e1822dc3bdc17b13ed65f95cf344fa15c
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-03-08 12:02:18 -0800

    added general templates folder

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+  path('/', views.index_view ),
+  path('/about', views.about_view),
+  path('/contact', views.contact_view)
+]
---
+++ website/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index_view(request):
+  return render(request, 'website/index.html')
+
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+def contact_view(request):
+   return render(request,'website/contact')
+
