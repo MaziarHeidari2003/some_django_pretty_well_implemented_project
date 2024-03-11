d721407b7eace36134d1b22b14253d2167ee1e38
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-03-11 04:46:49 -0700

    just started using admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+  title = models.CharField(max_length=255)
+  content = models.TextField()
+  counted_views = models.IntegerField(default=0)
+  status = models.BooleanField(default=False)
+  published_date = models.DateTimeField(null=True)
+  created_date = models.DateTimeField(auto_now_add=True)
+  updated_date = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return "{} - {}".format(self.title, self.id)
