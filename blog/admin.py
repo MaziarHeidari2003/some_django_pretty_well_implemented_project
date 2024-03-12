795708e00d89e7b2202671cbcbf0a19fa967b4f7
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-03-12 12:34:05 -0700

    just started createing custom tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 
 # Register your models here.
 
 class Post_admin(admin.ModelAdmin):
   date_hierarchy = 'created_date'
   empty_value_display = '-emptyBitch-'
-  list_display = ('title','author', 'content', 'published_date','created_date')
+  list_display = ('title','author','published_date','created_date')
   ordering = ['created_date']
   search_fields = ['title','content']
 
 
 
 admin.site.register(Post,Post_admin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name  
+
 class Post(models.Model):
-  image = models.ImageField(upload_to='blog/')
+  category = models.ManyToManyField(Category)
+  image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
   author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
   title = models.CharField(max_length=255)
   content = models.TextField()
@@ -19,3 +27,7 @@ class Post(models.Model):
 
   def __str__(self):
     return "{} - {}".format(self.title, self.id)
+  
+  def snippets(self):
+    return self.content[:150]
+  
