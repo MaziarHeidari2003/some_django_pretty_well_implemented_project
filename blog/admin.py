799a9df917a41453de5bc587a546f0bd48ebd4e3
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-03-12 04:19:53 -0700

    imagefield added

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from blog.models import Post
 class Post_admin(admin.ModelAdmin):
   date_hierarchy = 'created_date'
   empty_value_display = '-emptyBitch-'
-  list_display = ('title', 'content', 'published_date','created_date')
+  list_display = ('title','author', 'content', 'published_date','created_date')
   ordering = ['created_date']
   search_fields = ['title','content']
 
--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
+  image = models.ImageField(upload_to='blog/')
   author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
   title = models.CharField(max_length=255)
   content = models.TextField()
